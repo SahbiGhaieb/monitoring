65e001d2962f810e4bb58e3438bbaeb45fd31e16
mohamed sahbi ghaieb <mohamedsahbi.ghaieb@esprit.tn>
2019-10-25 19:40:35 +0200

    quelque correctifs

--- monit.sh
+++ monit.sh
@@ -7,29 +7,23 @@ fi
 #tester si le fichier est plain ou vide
 if [ -s /home/sahbi/Documents/testSysteme/log.txt ] 
 then
-    #echo "le fichier est plain"
-    end=$((SECONDS+3600))
-    while [ $SECONDS -lt $end ]; do
-    MEMORY=$(free -m | awk 'NR==2{printf "%.2f%%\t\t", $3*100/$2 }')
-    DISK=$(df -h | awk '$NF=="/"{printf "%s\t\t", $5}')
-    CPU=$(top -bn1 | grep load | awk '{printf "%.2f%%\t\t\n", $(NF-2)}')
-    echo "$MEMORY$DISK$CPU" >> /home/sahbi/Documents/testSysteme/log.txt
-    #envoyer un email
-    #echo -e "CPU/MEMORY/DISK:" | /usr/sbin/sendmail -f$from -t$to -u$subject -m"test" -a/home/sahbi/Documents/testSysteme/log.txt
-    #executer le script toute les 5minutes ou on peux modifier le crontab et ajouter une ligne
-    #sleep 300
-    sleep 5
-    done    
-    else
-    #echo "le fichier est vide"
-    printf "Memory\t\tDisk\t\tCPU\n" > "/home/sahbi/Documents/testSysteme/log.txt"
-        end=$((SECONDS+3600))
-        while [ $SECONDS -lt $end ]; do
-        DATE=$(date +"%m-%d-%Y-%T")
+    while [ true ]; do
         MEMORY=$(free -m | awk 'NR==2{printf "%.2f%%\t\t", $3*100/$2 }')
         DISK=$(df -h | awk '$NF=="/"{printf "%s\t\t", $5}')
         CPU=$(top -bn1 | grep load | awk '{printf "%.2f%%\t\t\n", $(NF-2)}')
         echo "$MEMORY$DISK$CPU" >> /home/sahbi/Documents/testSysteme/log.txt
-        sleep 5
+        #executer le script toute les 5minutes ou on peux modifier le crontab 
+        #et ajouter une ligne au lieu de cette commande
+        sleep 5m
+    done    
+    else
+    printf "Memory\t\tDisk\t\tCPU\n" > "/home/sahbi/Documents/testSysteme/log.txt"
+        while [ true ]; do
+            DATE=$(date +"%m-%d-%Y-%T")
+            MEMORY=$(free -m | awk 'NR==2{printf "%.2f%%\t\t", $3*100/$2 }')
+            DISK=$(df -h | awk '$NF=="/"{printf "%s\t\t", $5}')
+            CPU=$(top -bn1 | grep load | awk '{printf "%.2f%%\t\t\n", $(NF-2)}')
+            echo "$MEMORY$DISK$CPU" >> /home/sahbi/Documents/testSysteme/log.txt
+            sleep 5m
         done  
 fi
